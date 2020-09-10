6a17e02ce6950789580622c59f4be50bce763b54
ALLIOT21 <35888295+ALLIOT21@users.noreply.github.com>
2020-09-11 00:51:12 +0300

    Add project files.

---
+++ ReplyAlfaSoft/ColorNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ReplyAlfaSoft
+{
+    class ColorNode
+    {
+        public string Color { get; private set; }
+
+        public List<ColorNode> ColorNodeChilds { get; private set; }
+
+        public ColorNode(string color, List<ColorNode> colorNodeChilds = null)
+        {
+            Color = color;
+            ColorNodeChilds = colorNodeChilds;
+        }
+    }
+}
---
+++ ReplyAlfaSoft/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReplyAlfaSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yellowNode = new ColorNode("Желтый");
+            var orangeNode = new ColorNode("Оранжевый");
+            var blueNode = new ColorNode("Голубой");
+            var darkBlueNode = new ColorNode("Синий", new List<ColorNode> { orangeNode, yellowNode});
+            var redNode = new ColorNode("Красный");
+            var purpleNode = new ColorNode("Фиолетовый", new List<ColorNode> { blueNode });
+            var greenNode = new ColorNode("Зеленый", new List<ColorNode> { darkBlueNode, redNode, purpleNode });
+
+            var A = greenNode;            
+
+            Console.WriteLine(A);
+            Console.ReadKey();
+        }
+    }
+}
